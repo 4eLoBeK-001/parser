2d89293040040568cf1ae34330d656a0e16e2f25
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-04-26 13:54:45 +0500

    article list

--- data_aggregator/aggregator_app/views.py
+++ data_aggregator/aggregator_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Article
 # Create your views here.
+
+def article_list(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'aggregator_app/article_list.html', context)
