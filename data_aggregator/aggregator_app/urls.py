58e28288c7d60f08efb0802424f7768d3b961c85
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-04-30 14:54:04 +0000

    refac

--- data_aggregator/aggregator_app/urls.py
+++ data_aggregator/aggregator_app/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list),
-    path('habr/', views.habr_article_list),
-    path('vc/', views.vc_article_list),
 
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
     path('article/<int:article_id>/upvote/', views.article_upvote, name='article_upvote'),
--- data_aggregator/aggregator_app/views.py
+++ data_aggregator/aggregator_app/views.py
@@ -24,23 +24,6 @@ def article_list(request):
     }
     return render(request, 'aggregator_app/article_list.html', context)
 
-def habr_article_list(request):
-    articles = Article.objects.filter(source='Habr').order_by('-publication_date')
-    context = {
-        'articles': articles,
-        'source': Article.SourceChoice.HABR
-    }
-    return render(request, 'aggregator_app/habr_article_list.html', context)
-
-
-def vc_article_list(request):
-    articles = Article.objects.filter(source='Vc').order_by('-publication_date')
-    context = {
-        'articles': articles,
-        'source': Article.SourceChoice.VC
-    }
-    return render(request, 'aggregator_app/vc_article_list.html', context)
-
 
 def article_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
