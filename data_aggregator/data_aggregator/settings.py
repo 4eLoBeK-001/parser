db766dff74f0b7fbdccc880ba5b743455df713b3
4eLoBeK-001 <kostantin.ef@gmail.com>
2025-05-01 17:08:27 +0000

    configuration of the dockerfile and dockercompose

--- data_aggregator/data_aggregator/settings.py
+++ data_aggregator/data_aggregator/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'dbPostgres',
         'USER': 'CustomUser',
         'PASSWORD': 'postgress1',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
@@ -131,12 +131,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/1"
+CELERY_BROKER_URL = "redis://redis:6379/1"
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
